c102e0111c8189d8ce7e6cb718246d82d7446162
AmirAli Fallahi <amirfallahi222@gmail.com>
2025-11-12 14:43:15 +0330

    Added daily challenges

---
+++ Hackerrank/Week 1/01-PlusMinus/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'plusMinus' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void plusMinus(List<int> arr)
+    {
+        int pos = 0; int neg = 0; int zero = 0;
+
+        foreach (var i in arr)
+        {
+            if (i > 0) pos++;
+            else if (i < 0) neg++;
+            else zero++;
+        }
+
+        Console.WriteLine(((decimal)pos / arr.Count).ToString("N6"));
+        Console.WriteLine(((decimal)neg / arr.Count).ToString("N6"));
+        Console.WriteLine(((decimal)zero / arr.Count).ToString("N6"));
+
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.plusMinus(arr);
+    }
+}
---
+++ Hackerrank/Week 1/03-TimeConversion/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'timeConversion' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static string timeConversion(string s)
+    {
+        StringBuilder temp = new StringBuilder();
+        string time = s.Substring(0, s.Length - 2);
+        string format = s.Substring(s.Length - 2);
+
+        int hour = Int32.Parse(time.Substring(0, 2));
+        string rest = time.Substring(2);
+
+        if (format.ToLower().Equals("am"))
+        {
+            if (hour == 12) hour = 0;
+        }
+        else if (format.ToLower().Equals("pm"))
+        {
+            if (hour != 12)
+                hour += 12;
+        }
+        return temp.Append($"{hour:00}").Append(rest).ToString();
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string s = Console.ReadLine();
+
+        string result = Result.timeConversion(s);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
