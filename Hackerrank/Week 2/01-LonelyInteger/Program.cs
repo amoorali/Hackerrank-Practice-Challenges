25b2f77db965a9c3cbe73e30715382494a3dac9d
AmirAli Fallahi <amirfallahi222@gmail.com>
2025-11-16 08:50:17 +0330

    Week 2 - 1st Challenge

---
+++ Hackerrank/Week 2/01-LonelyInteger/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'lonelyinteger' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    public static int lonelyinteger(List<int> a)
+    {
+        var freq = new Dictionary<int, int>();
+        for (int i = 0; i < a.Count; i++)
+        {
+            if (freq.ContainsKey(a[i]))
+                freq[a[i]]++;
+            else
+                freq[a[i]] = 1;
+        }
+
+        for (int i = 0; i < freq.Count; i++)
+        {
+            var tmp = freq.ElementAt(i);
+            if (tmp.Value == 1)
+                return tmp.Key;
+        }
+
+        return 0;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+        int result = Result.lonelyinteger(a);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
