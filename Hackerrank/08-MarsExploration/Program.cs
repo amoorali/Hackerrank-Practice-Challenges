c3ed3aa0134d1e7bd0f3f1b940ee0bc53293c920
AmirAli Fallahi <amirfallahi222@gmail.com>
2025-11-20 13:43:42 +0330

    Week 2 - 6th to 8th Challenges

---
+++ Hackerrank/08-MarsExploration/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'marsExploration' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static int marsExploration(string s)
+    {
+        var realMessageLetters = "SOS".ToCharArray();
+        var changedLettersCount = 0;
+        var wordsCount = s.Count() / 3;
+
+        for (int i = 0; i < wordsCount; i++)
+        {
+            var word = s.Skip(i * 3).Take(3).ToArray();
+
+            for (int index = 0; index < 3; index++)
+            {
+                if (word[index] != realMessageLetters[index])
+                    changedLettersCount++;
+            }
+        }
+
+        return changedLettersCount;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string s = Console.ReadLine();
+
+        int result = Result.marsExploration(s);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
