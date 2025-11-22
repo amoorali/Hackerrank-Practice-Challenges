0e54e126c15a85aa9ef32fee07682f9f5a366682
AmirAli Fallahi <amirfallahi222@gmail.com>
2025-11-22 10:18:52 +0330

    Week 3 - 1st to 3rd Challenges

---
+++ Hackerrank/Week 3/03-XORStrings3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Solution
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            var strings = new string[2];
+
+            for (int i = 0; i < 2; i++)
+                strings[i] = Console.ReadLine();
+
+            var result = StringsXOR(strings);
+            Console.WriteLine(result);
+        }
+
+        static string StringsXOR(string[] strings)
+        {
+            var s1 = strings[0];
+            var s2 = strings[1];
+            var result = new StringBuilder();
+
+            for (int i = 0; i < s1.Length; i++)
+            {
+                if (s1[i] == s2[i])
+                    result.Append(0);
+                else
+                    result.Append(1);
+            }
+
+            return result.ToString();
+        }
+
+        static string StringsXOR2(string[] strings)
+        {
+            var s1 = strings[0];
+            var s2 = strings[1];
+
+            return new string(s1.Zip(s2, (num1, num2) => num1 == num2 ? '0' : '1').ToArray());
+        }
+    }
+}
