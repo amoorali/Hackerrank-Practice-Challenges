92551b0f505b955e5b9aedc50cd78bf8c4133775
AmirAli Fallahi <amirfallahi222@gmail.com>
2025-11-23 13:55:36 +0330

    Week 3 - 4th and 5th Challenges

---
+++ Hackerrank/Week 3/04-SalesByMatch/Program.cs
@@ -0,0 +1,79 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'sockMerchant' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER_ARRAY ar
+     */
+
+    public static int sockMerchant(int n, List<int> ar)
+    {
+        var pairs = 0;
+        var socksFrequency = new Dictionary<int, int>();
+
+        foreach (var sock in ar)
+        {
+            if (socksFrequency.ContainsKey(sock))
+                socksFrequency[sock]++;
+            else
+                socksFrequency[sock] = 1;
+        }
+
+        foreach (var sockCountPair in socksFrequency)
+        {
+            var count = sockCountPair.Value;
+
+            pairs += (count / 2);
+        }
+
+        return pairs;
+    }
+
+    public static int SockMerchant(int n, List<int> ar)
+    {
+        var frequency = ar.GroupBy<int, int>(sock => sock).ToList();
+        var pairsCount = 0;
+
+        frequency.ForEach(p => pairsCount += p.ToList().Count / 2);
+
+        return pairsCount;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt32(arTemp)).ToList();
+
+        int result = Result.sockMerchant(n, ar);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
---
+++ Hackerrank/Week 3/05-MigratoryBirds/Program.cs
@@ -0,0 +1,88 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'migratoryBirds' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int migratoryBirds(List<int> arr)
+    {
+        var frequency = new Dictionary<int, int>();
+        arr.Sort();
+
+        foreach (int birdType in arr)
+        {
+            if (frequency.ContainsKey(birdType))
+                frequency[birdType]++;
+            else
+                frequency[birdType] = 1;
+        }
+
+        var mostFrequent = frequency.MaxBy(p => p.Value).Key;
+
+        return mostFrequent;
+    }
+
+    public static int MigratoryBirds(List<int> arr)
+    {
+        var mostFrequentType = 0;
+        var mostFrequentCount = 0;
+
+        arr.GroupBy(n => n)
+            .ToList()
+            .ForEach(g =>
+                {
+                    int birdCount = g.ToList().Count;
+                    int birdType = g.First();
+                    Console.WriteLine(birdCount);
+
+                    if (birdCount > mostFrequentCount)
+                    {
+                        mostFrequentCount = birdCount;
+                        mostFrequentType = birdType;
+                    }
+                    if (birdCount == mostFrequentCount)
+                        mostFrequentType = birdType < mostFrequentType ? birdType : mostFrequentType;
+                }
+            );
+
+        return mostFrequentType;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int arrCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        int result = Result.migratoryBirds(arr);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
