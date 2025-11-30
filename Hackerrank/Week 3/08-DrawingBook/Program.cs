2d0b30103e3dd190cd97cf129c23d5a7fce1ca1d
AmirAli Fallahi <amirfallahi222@gmail.com>
2025-11-30 11:48:10 +0330

    Week 3 - 8th Challenge

---
+++ Hackerrank/Week 3/08-DrawingBook/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'pageCount' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER p
+     */
+
+    public static int pageCount(int n, int p)
+    {
+        long countFromFront = p / 2;
+        long countFromBack = (n / 2) - (p / 2);
+
+        Console.WriteLine($"{countFromFront}, {countFromBack}");
+
+        return (int)Math.Min(countFromFront, countFromBack);
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        int p = Convert.ToInt32(Console.ReadLine().Trim());
+
+        int result = Result.pageCount(n, p);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
