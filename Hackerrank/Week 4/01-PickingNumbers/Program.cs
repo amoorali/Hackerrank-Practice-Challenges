048286d391f4bd8dcec63b53a4204b2b9a12ca03
AmirAli Fallahi <amirfallahi222@gmail.com>
2025-12-06 10:47:49 +0330

    Week 4 - 1st to 4th Challenges

---
+++ Hackerrank/Week 4/01-PickingNumbers/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'pickingNumbers' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    public static int pickingNumbers(List<int> a)
+    {
+        int max = 0;
+        var frequencies = new int[100];
+
+        foreach (int number in a)
+            frequencies[number]++;
+
+        for (int i = 0; i < frequencies.Length - 1; i++)
+        {
+            max = Math.Max(frequencies[i] + frequencies[i + 1], max);
+        }
+
+        return max;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+        int result = Result.pickingNumbers(a);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
---
+++ Hackerrank/Week 4/04-SeperateTheNumbers/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'separateNumbers' function below.
+     *
+     * The function accepts STRING s as parameter.
+     */
+
+    public static void separateNumbers(string s)
+    {
+        for (var index = 1; index <= s.Length / 2; index++)
+        {
+            if (s.Length == 1 || s[0] == '0')
+                break;
+
+            var subset = s.Substring(0, index);
+            var beautifulString = new StringBuilder();
+
+            if (subset == "0")
+                continue;
+
+            if (!long.TryParse(subset, out long startingNumber))
+                break;
+
+            for (var i = 0; beautifulString.Length < s.Length; i++)
+            {
+                beautifulString.Append(startingNumber++);
+            }
+
+            if (beautifulString.ToString() == s)
+            {
+                Console.WriteLine($"YES {subset}");
+                return;
+            }
+        }
+
+        Console.WriteLine("NO");
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int q = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int qItr = 0; qItr < q; qItr++)
+        {
+            string s = Console.ReadLine();
+
+            Result.separateNumbers(s);
+        }
+    }
+}
