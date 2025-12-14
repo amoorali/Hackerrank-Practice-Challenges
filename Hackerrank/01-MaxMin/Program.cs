5f79a23813260f8088f86bdbcbd597fcc98fe9d6
amoorali <amirfallahi222@gmail.com>
2025-12-14 12:04:24 +0330

    Week 5 - 1st Challenge

---
+++ Hackerrank/01-MaxMin/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'maxMin' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER k
+     *  2. INTEGER_ARRAY arr
+     */
+
+    public static int maxMin(int k, List<int> arr)
+    {
+        var minimumUnfairness = Int32.MaxValue;
+
+        arr.Sort();
+
+        for (int i = 0; i <= arr.Count - k; i++)
+        {
+            var minNumber = arr[i];
+            var maxNumber = arr[i + k - 1];
+            var unfairness = maxNumber - minNumber;
+
+            if (unfairness < minimumUnfairness)
+                minimumUnfairness = unfairness;
+        }
+
+        return minimumUnfairness;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        int k = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = new List<int>();
+
+        for (int i = 0; i < n; i++)
+        {
+            int arrItem = Convert.ToInt32(Console.ReadLine().Trim());
+            arr.Add(arrItem);
+        }
+
+        int result = Result.maxMin(k, arr);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
