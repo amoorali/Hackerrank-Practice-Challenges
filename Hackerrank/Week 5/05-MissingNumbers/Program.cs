9c8f822500eaa944eb53fce0f28b6c871afd015d
amoorali <amirfallahi222@gmail.com>
2025-12-21 09:55:47 +0330

    Week 5 - 5th and 6th Challenges

---
+++ Hackerrank/Week 5/05-MissingNumbers/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'missingNumbers' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY arr
+     *  2. INTEGER_ARRAY brr
+     */
+
+    public static List<int> missingNumbers(List<int> arr, List<int> brr)
+    {
+        var arr_frequency = Frequency(arr);
+        var brr_frequency = Frequency(brr);
+        var missing = new HashSet<int>();
+
+        foreach (var number in brr_frequency.Keys)
+        {
+            if (!arr_frequency.ContainsKey(number) ||
+                brr_frequency[number] != arr_frequency[number])
+            {
+                missing.Add(number);
+            }
+        }
+
+        return missing.Order().ToList();
+    }
+
+    public static Dictionary<int, int> Frequency(List<int> arr)
+        => arr
+            .GroupBy(i => i)
+            .ToDictionary(g => g.Key, g => g.Count());
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        int m = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> brr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(brrTemp => Convert.ToInt32(brrTemp)).ToList();
+
+        List<int> result = Result.missingNumbers(arr, brr);
+
+        textWriter.WriteLine(String.Join(" ", result));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
---
+++ Hackerrank/Week 5/06-TheFullCountingSort/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'countSort' function below.
+     *
+     * The function accepts 2D_STRING_ARRAY arr as parameter.
+     */
+
+    public static void countSort(List<List<string>> arr)
+    {
+        var result = arr.Select((x, i) => new { Key = int.Parse(x[0]), Val = (i < arr.Count / 2) ? "-" : x[1] })
+                        .OrderBy(x => x.Key)
+                        .Select(x => x.Val);
+
+        Console.WriteLine(string.Join(" ", result));
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<List<string>> arr = new List<List<string>>();
+
+        for (int i = 0; i < n; i++)
+        {
+            arr.Add(Console.ReadLine().TrimEnd().Split(' ').ToList());
+        }
+
+        Result.countSort(arr);
+    }
+}
