ed768f3a3c1c96b33bea7202432107ed860b143a
amoorali <amirfallahi222@gmail.com>
2025-12-22 11:22:04 +0330

    Week 5 - 7th and 8th Challenges

---
+++ Hackerrank/Week 5/07-GridChallenge/Program.cs
@@ -0,0 +1,76 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'gridChallenge' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING_ARRAY grid as parameter.
+     */
+
+    public static string gridChallenge(List<string> grid)
+    {
+        var sortedGrid = grid.Select(row =>
+            string.Concat(row.OrderBy(c => c)))
+            .ToList();
+
+        var rows = grid.Count;
+        var columns = grid[0].Length;
+
+        for (int row = 1; row < rows; row++)
+        {
+            for (int column = 0; column < columns; column++)
+            {
+                if (sortedGrid[row][column] < sortedGrid[row - 1][column])
+                    return "NO";
+            }
+        }
+
+        return "YES";
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int t = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<string> grid = new List<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string gridItem = Console.ReadLine();
+                grid.Add(gridItem);
+            }
+
+            string result = Result.gridChallenge(grid);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
