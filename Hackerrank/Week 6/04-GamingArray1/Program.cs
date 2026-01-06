5de642012dc650c4e15d928a26172642c37b50d6
AmirAli Fallahi <amirfallahi222@gmail.com>
2026-01-06 15:33:38 +0330

    Week 6 - 4th Challenge

---
+++ Hackerrank/Week 6/04-GamingArray1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'gamingArray' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static string GamingArray(List<int> arr)
+    {
+        var turnCounter = 0;
+        int currentMax = int.MinValue;
+
+        foreach (var number in arr)
+        {
+            if (number > currentMax)
+            {
+                currentMax = number;
+                turnCounter++;
+            }
+        }
+
+        return turnCounter % 2 == 0 ? "ANDY" : "BOB";
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int g = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int gItr = 0; gItr < g; gItr++)
+        {
+            int arrCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+            string result = Result.GamingArray(arr);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
